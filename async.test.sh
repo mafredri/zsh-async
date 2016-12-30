mafredri/zsh-async

commit df61b12491e37a02647431438850a37ec7f63b4f

Properly parse command arguments in async worker
By separating all command arguments with a NULL-character, we can send
them to the async worker without losing new-lines or spaces. This
implementation relies on the fact that NULLs `$'\0'` are never used in
a call to `async_job`.

Fixes #8.

async.zsh
@@ -26,7 +26,7 @@ _async_job() {
 	unset stdout stderr ret
 	eval "$(
 		{
-			stdout=$(eval "$@")
+			stdout=$(eval '$@')
 			ret=$?
 			typeset -p stdout ret
 		} 2> >(stderr=$(cat); typeset -p stderr)
@@ -91,9 +91,21 @@ _async_worker() {
 		esac
 	done
 
-	while read -r cmd; do
-		# Separate on spaces into an array
-		cmd=(${=cmd})
+	local -a buffer
+	# Command arguments are separated with a null character.
+	while read -r -d $'\0' line; do
+		if [[ $line != ___ZSH_ASNYC_EOC___ ]]; then
+			# Read command arguments until we receive magic end-of-command string.
+			buffer+=($line)
+			continue
+		fi
+
+		# Copy command buffer
+		cmd=("${(@)=buffer}")
+
+		# Reset command buffer
+		buffer=()
+
 		local job=$cmd[1]
 
 		# Check for non-job commands sent to worker
@@ -223,7 +235,14 @@ async_job() {
 	setopt localoptions noshwordsplit
 
 	local worker=$1; shift
-	zpty -w $worker $@
+
+	local cmd p
+	for p in "$@"; do
+		cmd+="$p"$'\0'
+	done
+	cmd+=___ZSH_ASNYC_EOC___$'\0'
+
+	zpty -w $worker $cmd
 }
 
 # This function traps notification signals and calls all registered callbacks
@@ -283,7 +302,7 @@ async_flush_jobs() {
 	zpty -t $worker &>/dev/null || return 1
 
 	# Send kill command to worker
-	zpty -w $worker "_killjobs"
+	async_job $worker "_killjobs"
 
 	# Clear all output buffers
 	while zpty -r $worker line; do true; done

commit 23e5e6e25c2a091756fece2b00c08816c0d7c789

Make combined jobs (sleep 1; echo hello) work again (#9)
Fixes a regression caused by df61b12.

async.zsh
@@ -26,7 +26,7 @@ _async_job() {
 	unset stdout stderr ret
 	eval "$(
 		{
-			stdout=$(eval '$@')
+			stdout=$(eval "$@")
 			ret=$?
 			typeset -p stdout ret
 		} 2> >(stderr=$(cat); typeset -p stderr)

async.test.sh
@@ -70,6 +70,11 @@ async_job async git status --porcelain
 sleep 0.2
 async_process_results async simple_result
 
+# Test combined jobs
+async_job async 'sleep 0.1; simple_echo'
+sleep 0.2
+async_process_results async simple_result
+
 # Test uniqueness
 async_start_worker async2 -u
 # Only the first one will run!