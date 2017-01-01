510a77228b7863fb1801746b1fd07ad96a743d38
Mathias Fredriksson <mafredri@gmail.com>
2017-01-01 17:47:16 +0200

    Rewrite test runner

--- async_test.zsh
+++ async_test.zsh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-zmodload zsh/datetime
-
 test__async_job_print_hi() {
 	coproc cat
 	print -p t  # Insert token into coproc.
@@ -10,9 +8,9 @@ test__async_job_print_hi() {
 	local out
 	out=($(_async_job print hi))
 
-	print $out
-
-	[[ $out[1] == print ]] && [[ $out[2] == 0 ]] && [[ $out[3] = hi ]]
+	[[ $out[1] = print ]] || t_error "command name should be print, got" $out[1]
+	[[ $out[2] = 0 ]] || t_error "want exit code 0, got" $out[2]
+	[[ $out[3] = hi ]] || t_error "want output: hi, got" $out[3]
 }
 
 test__async_job_stderr() {
@@ -23,15 +21,16 @@ test__async_job_stderr() {
 	local out
 	out=($(_async_job 'print hi 1>&2'))
 
-	print $out
-
-	[[ $out[2] == 0 ]] && [[ -z $out[3] ]] && [[ $out[5] = hi ]]
+	[[ $out[2] = 0 ]] || t_error "want status 0, got" $out[2]
+	[[ -z $out[3] ]] || t_error "want empty output, got" $out[3]
+	[[ $out[5] = hi ]] || t_error "want stderr: hi, got" $out[5]
 }
 
 test__async_job_wait_for_token() {
+	float start duration
 	coproc cat
 
-	_async_job print hi &
+	_async_job print hi >/dev/null &
 	job=$!
 	start=$EPOCHREALTIME
 	{
@@ -41,8 +40,9 @@ test__async_job_wait_for_token() {
 
 	wait $job
 
+	duration=$(( EPOCHREALTIME - start ))
 	# Fail if the execution time was faster than 0.1 seconds.
-	return $(( (EPOCHREALTIME - start) <= 0.1 ))
+	(( duration > 0.1 )) || t_error "execution was too fast, want > 0.1, got" $duration
 }
 
 test__async_job_multiple_commands() {
@@ -53,12 +53,10 @@ test__async_job_multiple_commands() {
 	local out
 	out=($(_async_job 'print -n hi; for i in "1 2" 3 4; do print -n $i; done'))
 
-	print $out
-
 	# $out[1] here will be the entire string passed to _async_job()
 	# ('print -n hi...') since proper command parsing is done by
 	# the async worker.
-	[[ $out[3] = "hi1 234" ]]
+	[[ $out[3] = "hi1 234" ]] || t_error "want output hi1 234, got " $out[3]
 }
 
 test_async_job_multiple_commands_in_string() {
@@ -71,9 +69,8 @@ test_async_job_multiple_commands_in_string() {
 		sleep 0.1
 	done
 
-	print $result
-
-	[[ $result[1] = print ]] && [[ $result[3] = "hi  123 bye" ]]
+	[[ $result[1] = print ]] || t_error "want command name: print, got" $result[1]
+	[[ $result[3] = "hi  123 bye" ]] || t_error 'want output: "hi  123 bye", got' ${(qqq)result[3]}
 }
 
 test_async_job_git_status() {
@@ -86,9 +83,12 @@ test_async_job_git_status() {
 		sleep 0.1
 	done
 
-	print $result
+	[[ $result[1] = git ]] || t_error "want command name: git, got" $result[1]
+	[[ $result[2] = 0 ]] || t_error "want exit code: 0, got" $result[2]
 
-	[[ $result[1] = git ]] && [[ $result[2] == 0 ]] && [[ $result[3] == $(git status --porcelain) ]]
+	want=$(git status --porcelain)
+	got=$result[3]
+	[[ $got = $want ]] || t_error "want ${(qqq)want}, got ${(qqq)got}"
 }
 
 test_async_job_multiple_arguments_and_spaces() {
@@ -101,9 +101,12 @@ test_async_job_multiple_arguments_and_spaces() {
 		sleep 0.1
 	done
 
-	print $result
+	[[ $result[1] = print ]] || t_error "want command name: print, got" $result[1]
+	[[ $result[2] = 0 ]] || t_error "want exit code: 0, got" $result[2]
 
-	[[ $result[1] = print ]] && [[ $result[2] == 0 ]] && [[ $result[3] == "hello   world" ]]
+	[[ $result[3] = "hello   world" ]] || {
+		t_error "want output: \"hello   world\", got" ${(qqq)result[3]}
+	}
 }
 
 test_async_job_unique_worker() {
@@ -135,8 +138,14 @@ test_async_job_unique_worker() {
 	sleep 0.1
 	async_process_results test cb
 
-	print $result
-
 	# Ensure that cb was only called once with correc output.
-	[[ ${#result} = 5 ]] && [[ $result[3] == one ]]
+	[[ ${#result} = 5 ]] || t_error "result: want 5 elements, got" ${#result}
+	[[ $result[3] = one ]] || t_error "output: want \"one\", got" ${(qqq)result[3]}
+}
+
+test_main() {
+	# Load zsh-async before running each test.
+	zmodload zsh/datetime
+	source async.zsh
+	async_init
 }
--- test.zsh
+++ test.zsh
@@ -4,84 +4,184 @@
 # Checks for test files named *_test.zsh or *_test.sh and runs all functions
 # named test_*.
 #
-# usage:
-#   ./test.zsh
-#
 emulate -L zsh
 
 zmodload zsh/datetime
+zmodload zsh/parameter
+zmodload zsh/zutil
 
-source **/*_test.(zsh|sh)
+TEST_GLOB=.
+TEST_VERBOSE=0
+TEST_CODE_SKIP=101
+TEST_CODE_ERROR=100
+
+show_help() {
+	print "usage: ./test.zsh [-v] [search glob]"
+}
+
+parse_opts() {
+	local -a verbose help
+	zparseopts -E -D \
+		v=verbose -verbose=verbose \
+		h=help -help=help \
+		\?=help
+
+	(( $+help[1] )) && show_help && exit 0
+
+	if (( $#@ > 1 )); then
+		print -- "unknown arguments: $@"
+		show_help
+		exit 1
+	fi
+
+	[[ -n $1 ]] && TEST_GLOB=$1/
+	TEST_VERBOSE=$+verbose[1]
+}
+
+# t_log is for printing log output, visible in verbose (-v) mode.
+t_log() {
+	print -u 3 -- $'\t'"${funcfiletrace[1]}: $@"
+}
+
+# t_skip is for skipping a test.
+t_skip() {
+	t_log $@
+	exit $TEST_CODE_SKIP
+}
+
+typeset -g -a err_lines
+# t_error logs the error and fails the test without aborting.
+t_error() {
+	# Store the function line that produced the error in
+	# err_lines, used to figure out if an error happened.
+	err_lines+=(${${(s.:.)functrace[1]}[2]})
+	t_log $@
+}
+
+# t_fatal logs the error and immediately fails the test.
+t_fatal() {
+	t_log $@
+	exit $TEST_CODE_ERROR
+}
 
 # run_test runs the test function and reports it's status.
 run_test() {
-	local ret
+	local code
 
-	float start=$EPOCHREALTIME duration
-	out="$(
-		# Initialize async.
-		source ./async.zsh
-		async_init
+	# Manage stdout / stderr.
+	exec 3>&1
+	if (( ! TEST_VERBOSE )); then
+		exec 4>/dev/null
+	else
+		exec 4>&1
+	fi
+	# Run the test.
+	$t 1>&4 2>&3
+	code=$?
+	(( ${#err_lines} )) && return $TEST_CODE_ERROR
+	return $code
+}
 
-		# Run the test.
-		$t 2>&1
-	)"
-	ret=$?
+# run_test_module runs all the tests from a test module (asynchronously).
+run_test_module() {
+	local module=$1 num_tests=0
+	local -a tests
+	float start duration
+
+	source $module
+
+	# Find all functions named test_* (sorted), excluding test_main.
+	tests=(${(R)${(okM)functions:#test_*}:#test_main})
+	num_tests=${#tests}
+
+	# Run test_main.
+	if [[ -n $functions[test_main] ]]; then
+		test_main
+	fi
+
+	start=$EPOCHREALTIME
+
+	# Open coproc for test results.
+	coproc cat
+
+	# Run all the tests asynchronously.
+	local i=0
+	for t in $tests; do
+		(( i++ ))
+		{
+			local code out
+			float start=$EPOCHREALTIME duration
+			out="$(run_test $i $t)"
+			code=$?
+			duration=$(( EPOCHREALTIME - start ))
+			print -r -p $i $code $duration "${(q)out}"$'\0'
+		} &
+	done
+
+	local code
+	local -A codes times outputs
+	# Parse all test results from coproc.
+	(( i > 0 )) && while read -r -d $'\0' -A -p line; do
+		code=$line[1]; shift line
+		codes+=($code $line[1])
+		times+=($code $line[2])
+		shift 2 line
+		outputs+=($code "${(Q)line}")
+
+		# Check if we've received all results.
+		(( ${#codes} == i )) && break
+	done
+
+	coproc exit
+
 	duration=$(( EPOCHREALTIME - start ))
 
-	# Print everything at once to avoid garbled text.
-	print "$(
-		print -- "=== RUN   $t"
-		if (( ret == 0 )); then
-			print -- "--- PASS: $t $(printf "(%.2fs)" $duration)"
-		else
-			print -- "--- FAIL: $t"
-			print -- "\tstatus: $ret"
-			print -- "\toutput: $out"
+	local test state output show failed=0
+	for ti in ${(onk)codes}; do
+		state=PASS
+		test=$tests[$ti]
+		code=$codes[$ti]
+		output=$outputs[$ti]
+		show=$TEST_VERBOSE
+
+		if (( code == TEST_CODE_ERROR )); then
+			state=FAIL
+			show=1
+			(( failed++ ))
+		elif (( code == TEST_CODE_SKIP )); then
+			state=SKIP
 		fi
-	)"
 
-	print -p $ret
+		if (( !show )); then
+			continue
+		fi
+		print "=== RUN   $test"
+		print -n -- "--- $state: $test"
+		printf " (%.2fs)\n" $times[$ti]
+		[[ -n $output ]] && print $output
+	done
 
-	return $ret
+	if (( failed > 0 )); then
+		print "FAIL"
+		printf "FAIL\t$module\t%.3fs\n" $duration
+		return 1
+	fi
+
+	if (( TEST_VERBOSE )); then
+		print "PASS"
+	fi
+	printf "ok\t$module\t%.3fs\n" $duration
 }
 
-# Open coproc for receiving messages from run_test().
-coproc cat
+# Parse command arguments.
+parse_opts $@
 
-# Find all functions named test_*.
-tests=(${(okM)functions:#test_*})
-num_tests=${#tests}
-
-# Run all the tests asynchronously.
-for t in $tests; do
-	run_test $t &
+# Execute tests modules.
+failed=0
+for tf in ${~TEST_GLOB}/*_test.(zsh|sh); do
+	run_test_module $tf &
+	wait $!
+	(( $? )) && failed=1
 done
 
-fail=0
-codes=()
-(( num_tests > 0 )) && while read -p code; do
-	codes+=($code)
-
-	# Non-zero exit code from test, that's a fail!
-	(( code != 0 )) && fail=1
-
-	# Break if we've received an exit code from all tests.
-	(( ${#codes} == num_tests )) && break
-done
-
-# Close the message coproc.
-coproc :
-
-# Wait for all child processes to exit.
-wait
-
-# Just one failure, ruins everything 😢.
-(( fail )) && {
-	print "FAIL"
-	exit 1
-}
-
-# Green lights all the way, baby!
-print "PASS"
-exit 0
+exit $failed
