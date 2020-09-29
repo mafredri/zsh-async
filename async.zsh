bbbc92bd01592513a6b7739a45b7911af18acaef
Mathias Fredriksson <mafredri@gmail.com>
2020-09-29 13:50:34 +0300

    v1.8.5

--- async.zsh
+++ async.zsh
@@ -3,12 +3,12 @@
 #
 # zsh-async
 #
-# version: v1.8.4
+# version: v1.8.5
 # author: Mathias Fredriksson
 # url: https://github.com/mafredri/zsh-async
 #
 
-typeset -g ASYNC_VERSION=1.8.4
+typeset -g ASYNC_VERSION=1.8.5
 # Produce debug output from zsh-async when set to 1.
 typeset -g ASYNC_DEBUG=${ASYNC_DEBUG:-0}
 
